ac0be807838e9c27c6b83dbcffc3c8bc56a344f2
Tommy0710 <thiennguyen0710.dn@gmail.com>
2024-03-04 23:10:32 +0700

    init pizza2

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,7 +2,7 @@ import Image from "next/image"
 export default function Login(){
     return(
         <section className="max-w-6xl mx-auto">        
-            <h1>Login
+            <h1>Loginnn
                 
             </h1>
             
